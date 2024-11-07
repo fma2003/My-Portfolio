b5925cf9f307475834fd3794411666eb815b326c
Fahad Mohammad Ali <fma2003@hw.ac.uk>
2024-11-07 12:38:42 +0400

    update

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL, // Explicitly add environment variable
+  },
 };
 
 module.exports = nextConfig;
