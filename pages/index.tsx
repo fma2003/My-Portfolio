a05a1f2a65569def0e10965d980b44ebaffcec3c
Fahad Mohammad Ali <fma2003@hw.ac.uk>
2024-11-07 12:42:53 +0400

    update 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
@@ -116,7 +116,7 @@ const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
 
 export default Home;
 
-export const getStaticProps: GetStaticProps<Props> = async () => {
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
   const pageInfo = await fetchPageInfo();
   const experiences = await fetchExperiences();
   const skills = await fetchSkills();
